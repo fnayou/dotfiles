2378cac0137f5026bea31bae9a8643445c1a12c0
Aymen Fnayou <fnayou.aymen@gmail.com>
2026-06-21 05:27:49 +0200

    chore: prepare repo for public release
    - Relicense to MIT (was "all rights reserved", which blocked any public use)
    - Scrub personal email and home path from review doc 0033 -> placeholders
    - Move location-specific `meteo` alias out of committed aliases.zsh;
      document it as a commented example in local.zsh.example (kept private
      in git-ignored ~/.config/zsh/local.zsh)

--- stow/common/zsh/.config/zsh/aliases.zsh
+++ stow/common/zsh/.config/zsh/aliases.zsh
@@ -15,9 +15,6 @@ alias cp='cp -iv'
 alias rm='rm -i'
 alias mv='mv -iv'
 
-# --- Misc ---
-alias meteo='curl -4 http://wttr.in/Plaisir'
-
 # --- Suffix aliases (bat) ---
 if command -v bat >/dev/null 2>&1; then
   alias -s md=bat
